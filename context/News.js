d63709bca9be5690a7f8729aa00e6d990cce27df
dottor5 <dluccash@hotmail.com>
2020-09-20 21:31:15 -0300

    Initialize project using Create React App

---
+++ context/News.js
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect } from "react";
+
+const NewsContext = createContext();
+
+export default function NewsProvider({ children }) {
+  return <div></div>;
+}
+
+export function useNews() {}
