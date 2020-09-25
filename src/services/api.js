3b2c8f051b7d6b824d226312420150b0ba2f2ac3
dottor5 <dluccash@hotmail.com>
2020-09-24 23:26:20 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://newsapi.org/v2",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2",
   headers: { "X-API-KEY": "b7fb08c9f94f4898ae24c81664e9ae7c" },
 });
 
